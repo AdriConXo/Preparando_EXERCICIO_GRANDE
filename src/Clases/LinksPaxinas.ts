ca3e478762170bebbb7307b9c0cdc80936f19f99
AdriConXo <adri82cx@gmail.com>
2025-02-25 12:00:32 +0100

    Primer commit - Subiendo el proyecto

---
+++ src/Clases/LinksPaxinas.ts
@@ -0,0 +1,9 @@
+abstract class LinksPaxinas{
+   
+    abstract etiquetaDiv(): HTMLDivElement; // O contedor dos elementos
+    abstract etiquetaA() : HTMLAnchorElement;
+    abstract etiquetaImg() : HTMLImageElement;
+}
+
+export {LinksPaxinas}
+
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { NavegacionMenuPrincipal } from "./Clases/MenuPrincipalPC";
+
+
+
+// Utilizo a clase NavegacionMenuPrincipal
+// para crear o menú
+// INSTANCIO A CLASE DE NAVEGACIÓN DO MENÚ PRINCIPAL
+let navegar = new NavegacionMenuPrincipal();
+
+// 1º Na etiqueta 'a' creamos no atributo 'href=#'
+navegar.crearAtributos("#","./imaxes/codigo.png");
+// 2º Introducimos no atributo 'class' o estilo
+navegar.establecerEstilo("selector-a","test-selector-div","test-selector-img");
+navegar.introducirTextoNasEtiquetas("este é o enlace")
+navegar.pintarEnPantalla()
+
+
+
+
+
+
+
+/*
+let instanciaEtiquetaA = new EtiquetaA(document.createElement("a"))
+
+instanciaEtiquetaA.introducirAtributoHref("#")
+
+console.log(instanciaEtiquetaA.valorRefEtiquetaA)
+*/
+/* import { EtiquetaP } from "./Clases/EtiquetaP";
+import { PintarElemento } from "./Clases/PintarElemento";
+
+let unTexto = "por fín é VENRES!!!";
+
+// Creo a instancia a clase EtiquetaP
+let instanciaEtiquetaP = new EtiquetaP();
+
+// 1º Creo a referencia,neste caso a P
+let etiquetaPCreada = instanciaEtiquetaP.creoReferencia();
+
+// 2º Introduzco o texto 
+instanciaEtiquetaP.introduzcoTexto(unTexto);
+
+// 3º Creo a instancia a clase de Pintar no HTML
+let pintoNoHTML = new PintarElemento(document.body)
+
+// 4º Pinto no HTML
+pintoNoHTML.pintarOFinal(instanciaEtiquetaP.devolverReferencia()) */
+
+
